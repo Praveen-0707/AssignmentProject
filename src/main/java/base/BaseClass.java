54829c88c463218ca55743f5ce38bb6b27e45a32
HP <HP@DESKTOP-7PPMDVL>
2021-08-08 14:16:29 +0530

    code changes

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -1,9 +1,12 @@
 package base;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -15,6 +18,7 @@ public class BaseClass {
 	public RemoteWebDriver driver;
 	public ChromeOptions chromeOptions;
 	public static WebDriverWait wait;
+	public String remoteRun;
 	
 	private static ThreadLocal<RemoteWebDriver> localDriver = new ThreadLocal<RemoteWebDriver>();
 	
@@ -28,15 +32,36 @@ public class BaseClass {
 		return localDriver.get();
 	}
 	
+	public static final String URL = "https://oauth-praveen.beula77-586b7:9261db47-d810-4a8f-8c07-50a085624764@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+	
 	@BeforeMethod
 	public void launchApp()
 	{
+		remoteRun = "true";
+
+		DesiredCapabilities dcaps = new DesiredCapabilities();
+		dcaps.setCapability("browserName", "chrome");
+		dcaps.setCapability("browserVersion", "latest");
+		dcaps.setCapability("platformName", "Windows 10");
+		dcaps.setCapability("extendedDebugging", "true");
 		
-		WebDriverManager.chromedriver().setup();
-		chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--disable-notifications");
-		driver = new ChromeDriver(chromeOptions);
-		setDriver(driver);
+		if(remoteRun.equalsIgnoreCase("true"))
+			try {
+					WebDriverManager.chromedriver().setup();
+					dcaps.setCapability(ChromeOptions.CAPABILITY, setChromeOptions());
+					driver = new RemoteWebDriver(new URL(URL), dcaps);
+					setDriver(driver);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		else
+		{
+			WebDriverManager.chromedriver().setup();
+			chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--disable-notifications");
+			driver = new ChromeDriver(chromeOptions);
+			setDriver(driver);
+		}
 		getDriver().manage().deleteAllCookies();
 		getDriver().manage().window().maximize();
 		wait = new WebDriverWait(driver,Duration.ofSeconds(20));
@@ -45,6 +70,13 @@ public class BaseClass {
 		
 		String URL = "https://login.salesforce.com/";
 		getDriver().get(URL);
+	}
+	
+	public ChromeOptions setChromeOptions()
+	{
+		  chromeOptions = new ChromeOptions();
+		  chromeOptions.addArguments("--disable-notifications");
+		  return chromeOptions;
 	}
 	
 	@AfterMethod
